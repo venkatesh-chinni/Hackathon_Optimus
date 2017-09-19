c03050e4ca4ceb81a8241ab783caca2a2e55c32f
Baisakh Roy <baisakhkdj@gmail.com>
2017-09-19 17:05:00 +0530

    form commit

---
+++ src/app/submit.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { SubmitService } from './submit.service';
+
+
+@Component({
+    selector:'submitcomponent',
+    templateUrl: './submit.component.html',
+    styles:[`
+    `]
+})
+
+
+export class SubmitComponent{
+
+}
